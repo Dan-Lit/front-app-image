73cc5b7a1e6c7707d6274904ab58a619c90083c7
Daniel <danielpmacias@gmail.com>
2022-05-28 21:32:53 +0200

    Instalación npm galería

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Angular2ImageGalleryModule } from 'angular2-image-gallery';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    Angular2ImageGalleryModule,
     BrowserModule,
     AppRoutingModule
   ],
