6a7076ec653b12fca789998b3f73b1f86e5d4420
Daniel <74400053+Dan-Lit@users.noreply.github.com>
2022-06-12 22:20:38 +0200

    Eliminados métodos innecesarios (#11)

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -9,31 +9,18 @@ import { ImageResponse } from '../shared/image-response.model';
 })
 export class ConfigService {
   urlPath: string = 'https://localhost:7194';
-  imageID: string = 'dcada2fb-7d0d-416e-94a4-75fbc59b2f41';
 
   constructor(private client: HttpClient) {} 
 
-  public getAllImages() {
-    return this.client.get<ImageResponse>(this.urlPath + '/api/images/getall');
-  }
-
-  public getImage() {
-    return this.client.get<string>(this.urlPath + '/api/images/' + this.imageID);
-  }
-
   public uploadImage(formData: FormData): Observable<Object> {
     console.log(this.urlPath);
-    return this.client.put(this.urlPath + '/api/images/several', formData, {
+    return this.client.put(this.urlPath + '/api/images', formData, {
       observe: 'response',
     });
   }
 
   public getAllImagesURL() {
     return this.client.get<ImageModel>(this.urlPath + '/api/Images/GetAll/url');
-  }
-
-  public getImageTags(imageId: string) {
-    return this.client.get(this.urlPath + '/api/images/getTags/' + imageId);
   }
 
   public AssignTag(imageId: string, tagName: string) {
